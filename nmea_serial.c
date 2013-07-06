f50189bf175f1e31c14d538e0cfb6c5c175009cc
John Kearney <john.mark.kearney@gmail.com>
2013-07-06 10:22:40 -0700

    Infinite read loop...

--- nmea_serial.c
+++ nmea_serial.c
@@ -3,100 +3,102 @@
 //
 
 #include <stdio.h>
-#include <unistd.h>			//Used for UART
-#include <fcntl.h>			//Used for UART
-#include <termios.h>		//Used for UART
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+#define SERIAL_DEVICE "/dev/ttyAMA0"
+#define NMEA_MAX 82
+
+int setup_uart();
 
 int main() {
-	//-------------------------
-	//----- SETUP USART 0 -----
-	//-------------------------
-	//At bootup, pins 8 and 10 are already set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
-	int uart0_filestream = -1;
-	
-	//OPEN THE UART
-	//The flags (defined in fcntl.h):
-	//	Access modes (use 1 of these):
-	//		O_RDONLY - Open for reading only.
-	//		O_RDWR - Open for reading and writing.
-	//		O_WRONLY - Open for writing only.
-	//
-	//	O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
-	//											if there is no input immediately available (instead of blocking). Likewise, write requests can also return
-	//											immediately with a failure status if the output can't be written immediately.
-	//
-	//	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
-	uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
-	if (uart0_filestream == -1)
-	{
-		//ERROR - CAN'T OPEN SERIAL PORT
-		printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
-	}
-	
-	//CONFIGURE THE UART
-	//The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
-	//	Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
-	//	CSIZE:- CS5, CS6, CS7, CS8
-	//	CLOCAL - Ignore modem status lines
-	//	CREAD - Enable receiver
-	//	IGNPAR = Ignore characters with parity errors
-	//	ICRNL - Map CR to NL on input
-	//	PARENB - Parity enable
-	//	PARODD - Odd parity (else even)
-	struct termios options;
-	tcgetattr(uart0_filestream, &options);
-	options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;		//<Set baud rate
-	options.c_iflag = IGNPAR | ICRNL;
-	options.c_oflag = 0;
-	options.c_lflag = 0;
-	tcflush(uart0_filestream, TCIFLUSH);
-	tcsetattr(uart0_filestream, TCSANOW, &options);
+    int fd = setup_uart();
+    if (fd == -1) {
+        return 1;
+    }
 
-	//----- TX BYTES -----
-	unsigned char tx_buffer[20];
-	unsigned char *p_tx_buffer;
-	
-	p_tx_buffer = &tx_buffer[0];
-	*p_tx_buffer++ = 'H';
-	*p_tx_buffer++ = 'e';
-	*p_tx_buffer++ = 'l';
-	*p_tx_buffer++ = 'l';
-	*p_tx_buffer++ = 'o';
-	
-	if (uart0_filestream != -1)
-	{
-		int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));		//Filestream, bytes to write, number of bytes to write
-		if (count < 0)
-		{
-			printf("UART TX error\n");
-		}
-	}
+    //----- TX BYTES -----
+    //unsigned char tx_buffer[NMEA_MAX];
+    //memset(tx_buffer, 0, NMEA_MAX);
 
-	//----- CHECK FOR ANY RX BYTES -----
-	if (uart0_filestream != -1)
-	{
-		// Read up to 255 characters from the port if they are there
-		unsigned char rx_buffer[256];
-		int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
-		if (rx_length < 0)
-		{
-			//An error occured (will occur if there are no bytes)
-		}
-		else if (rx_length == 0)
-		{
-			//No data waiting
-		}
-		else
-		{
-			//Bytes received
-			rx_buffer[rx_length] = '\0';
-			printf("%i bytes read : %s\n", rx_length, rx_buffer);
-		}
-	}
+    //const char* data = "$JKMSG,*\r\n";
+    //memcpy(tx_buffer, data, sizeof(data));
+    
+    //int count = write(fd, tx_buffer, sizeof(data));
+    //if (count < 0) {
+    //    printf("UART TX error\n");
+    //}
 
+    for (;;) {
+        //----- CHECK FOR ANY RX BYTES -----
+        // Read up to 255 characters from the port if they are there
+        unsigned char rx_buffer[NMEA_MAX];
+        int rx_length = read(fd, rx_buffer, NMEA_MAX);
+        if (rx_length < 0) {
+            //An error occured (will occur if there are no bytes)
+        } else if (rx_length == 0) {
+            //No data waiting
+        } else {
+            //Bytes received
+            rx_buffer[rx_length] = '\0';
+            printf("%i bytes read : %s\n", rx_length, rx_buffer);
+        }
+    }
+    
 
-	//----- CLOSE THE UART -----
-	close(uart0_filestream);
+    //----- CLOSE THE UART -----
+    close(fd);
 
+    return 0;
 }
 
+int setup_uart() {
+    //-------------------------
+    //----- SETUP USART 0 -----
+    //-------------------------
+    //At bootup, pins 8 and 10 are already set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
+    //
+    //OPEN THE UART
+    //The flags (defined in fcntl.h):
+    //  Access modes (use 1 of these):
+    //      O_RDONLY - Open for reading only.
+    //      O_RDWR - Open for reading and writing.
+    //      O_WRONLY - Open for writing only.
+    //
+    //  O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
+    //                                          if there is no input immediately available (instead of blocking). Likewise, write requests can also return
+    //                                          immediately with a failure status if the output can't be written immediately.
+    //
+    //  O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
+    int fd = open(SERIAL_DEVICE, O_RDWR | O_NOCTTY | O_NDELAY);      //Open in non blocking read/write mode
+    if (fd == -1) {
+        printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
+        return -1;
+    }
+    
+    struct termios options;
+    if (tcgetattr(fd, &options) != 0) {
+        printf("error %d from tcgetattr", errno);
+        close(fd);
+        return -1;
+    }
+    options.c_cflag = B4800 | CS8 | CLOCAL | CREAD;
+    options.c_iflag = IGNPAR | ICRNL;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    if (tcflush(fd, TCIFLUSH) != 0) {
+        printf("error %d from tcflush", errno);
+        close(fd);
+        return -1;
+    }
+    if (tcsetattr(fd, TCSANOW, &options) != 0) {
+        printf("error %d from tcsetattr", errno);
+        close(fd);
+        return -1;
+    }
+
+    return fd;
+}
